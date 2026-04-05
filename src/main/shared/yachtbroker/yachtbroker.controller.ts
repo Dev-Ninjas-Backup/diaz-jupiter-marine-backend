c4443036efe3996fcdb7fcf6917456ffd8442b44
mirzasaikatahmmed <mirzasaikatahmmed@gmail.com>
2026-04-06 03:25:32 +0600

    ✨ feat: Enhance Boats.com & YachtBroker integrations

--- src/main/shared/yachtbroker/yachtbroker.controller.ts
+++ src/main/shared/yachtbroker/yachtbroker.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Param, Query } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { PaginationDto } from '@/common/dto/pagination.dto';
 import { YachtBrokerFilterDto } from './yachtbroker.dto';
 import { YachtBrokerService } from './yachtbroker.service';
 
@@ -12,6 +13,15 @@ export class YachtBrokerController {
   @ApiOperation({ summary: 'Get paginated list of YachtBroker listings' })
   async getAll(@Query() query: YachtBrokerFilterDto) {
     return this.yachtBrokerService.getAll(query);
+  }
+
+  @Get('ai')
+  @ApiOperation({
+    summary:
+      'Get YachtBroker listings in Inventory API format for AI consumption',
+  })
+  async getAiFormat(@Query() query: PaginationDto) {
+    return this.yachtBrokerService.getAiFormat(query);
   }
 
   @Get(':id')
